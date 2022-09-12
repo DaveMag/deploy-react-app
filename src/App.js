3ee75f00767d3067536049eb9346811c93ac6e47
SMD8 <davidemao86@gmail.com>
2022-09-12 19:06:34 +0200

    changing code

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Change the code</p>
+        <p>Code needs to change</p>
         <a
           className="App-link"
           href="https://reactjs.org"
